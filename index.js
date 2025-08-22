d2bf9f3e267829f7c11051e81052d5023e8efa6d
Nicolas <pikachugd68@gmail.com>
2025-08-22 20:52:33 -0300

    subir archivos

--- index.js
+++ index.js
@@ -29,68 +29,127 @@ miPromesa.then((valor) => {
 }) */
 
 const http = require('http');
-
 const path = require('path');
 const fs = require('fs');
 const port = 3000
+const url = require('url')
+const {IncomingForm, formidable} = require('formidable');
+
 
 // Crear un servidor HTTP que sirva un archivo HTML
 const server = http.createServer((req, res) => {
 
-    if (req.url === '/index.html' || req.url === '/' || req.url === '/index') {
-        const filePath = path.join(__dirname, './pages/index.html');
+    const parseUrl = url.parse(req.url, true)
+    const pathname = parseUrl.pathname;
 
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                console.error(err);
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end('Error al leer el archivo HTML');
+    switch (pathname){
+        case '/':
+            ServirHTML('index.html', res);
+            break;
+        case '/contact':
+            ServirHTML('contact.html', res)
+            break;
+        case '/about':
+            ServirHTML('about.html', res)
+            break;
+        case '/time':
+            mostrarhora(res)
+            break;
+        case '/upload':
+            if(req.method === 'GET'){
+                ServirHTML('SubirArch.html', res)
+            }if(req.method === 'POST'){
+                capturarArch(req, res);
             }
-            else {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(data);
-            }
-        });
-    } else if (req.url === '/about') {
-        const filePath = path.join(__dirname, './pages/about.html');
+            break;
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Ruta no encontrada');
+    }
+    // if (req.url === '/index.html' || req.url === '/' || req.url === '/index') {
+    //     const filePath = path.join(__dirname, './pages/index.html');
 
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                console.error(err);
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end('Error al leer el archivo HTML');
-            }
-            else {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(data);
-            }
-        });
-    } else if (req.url === '/time') {
-        // Responder con un mensaje de tiempo actual
-        res.end(JSON.stringify({Hora_Servidor: new Date().toISOString()}));
-    }
-    else if (req.url === '/contact') {
-        const filePath = path.join(__dirname, './pages/contact.html');
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                console.error(err);
-                res.writeHead(500, {'Content-Type': 'text/plain'});
-                res.end('Error al leer el archivo HTML');
-            }
-            else{
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(data);
-            }
-        }); }
-        else if (req.url == '/public'){
-            res.end(JSON.stringify({Public: 'Aca va el public'}));
-        }
-        else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Ruta no encontrada');
-    }
+    //     fs.readFile(filePath, (err, data) => {
+    //         if (err) {
+    //             console.error(err);
+    //             res.writeHead(500, { 'Content-Type': 'text/plain' });
+    //             res.end('Error al leer el archivo HTML');
+    //         }
+    //         else {
+    //             res.writeHead(200, { 'Content-Type': 'text/html' });
+    //             res.end(data);
+    //         }
+    //     });
+    // } else if (req.url === '/about') {
+    //     const filePath = path.join(__dirname, './pages/about.html');
+
+    //     fs.readFile(filePath, (err, data) => {
+    //         if (err) {
+    //             console.error(err);
+    //             res.writeHead(500, { 'Content-Type': 'text/plain' });
+    //             res.end('Error al leer el archivo HTML');
+    //         }
+    //         else {
+    //             res.writeHead(200, { 'Content-Type': 'text/html' });
+    //             res.end(data);
+    //         }
+    //     });
+    // } else if (req.url === '/time') {
+    //     res.end(JSON.stringify({Hora_Servidor: new Date().toISOString()}));
+    // }
+    // else if (req.url === '/contact') {
+    //     const filePath = path.join(__dirname, './pages/contact.html');
+    //     fs.readFile(filePath, (err, data) => {
+    //         if (err) {
+    //             console.error(err);
+    //             res.writeHead(500, {'Content-Type': 'text/plain'});
+    //             res.end('Error al leer el archivo HTML');
+    //         }
+    //         else{
+    //             res.writeHead(200, {'Content-Type': 'text/html'});
+    //             res.end(data);
+    //         }
+    //     }); }
+    //     else if (req.url == '/public'){
+    //         res.end(JSON.stringify({Public: 'Aca va el public'}));
+    //     }
+    //     else {
+    //     res.writeHead(404, { 'Content-Type': 'text/plain' });
+    //     res.end('Ruta no encontrada');
+    // }
 });
 
 server.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
 });
+
+function ServirHTML(NombreArchivo, res) {
+    const ruta = path.join('pages', NombreArchivo);
+    fs.readFile(ruta, 'utf8', (err, data) =>{
+        if(err){
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end(`Error interno: ${err}`)
+        }else{
+                res.writeHead(500, { 'Content-Type': 'text/html' });
+                res.end(data)
+        }
+    })
+}
+
+function mostrarhora (res){
+                res.end(JSON.stringify({Hora_Servidor: new Date().toISOString()}));
+}
+
+function capturarArch(req, res){
+    const form = formidable({ multiples: false, uploadDir: "./public", keepExtensions: true });
+    
+        form.parse(req, (err, fields, files) => {
+        if (err) {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Error en la subida");
+            return;
+        }
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ fields, files }, null, 2));
+        });
+}
