be6110fcc9821f061263879756082c233624d4af
Nicolas <pikachugd68@gmail.com>
2025-08-22 19:34:54 -0300

    2

--- index.js
+++ index.js
@@ -81,7 +81,11 @@ const server = http.createServer((req, res) => {
                 res.writeHead(200, {'Content-Type': 'text/html'});
                 res.end(data);
             }
-        }); } else {
+        }); }
+        else if (req.url == '/public'){
+            res.end(JSON.stringify({Public: 'Aca va el public'}));
+        }
+        else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('Ruta no encontrada');
     }
